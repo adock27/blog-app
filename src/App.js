dc51520f45ceb22e0a0a837d27c6877053332086
adock27 <andersondumet@gmail.com>
2023-04-14 00:27:33 -0500

    lazy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -12,31 +12,42 @@ import Login from './components/Login';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from './auth/authSlice';
 import ProtectedRoute from './components/ProtectedRoute';
-import Dashboard from './components/Dashboard';
+
 import { Link } from 'react-router-dom';
-import BlogListAdmin from './components/dashboard/BlogListAdmin';
+
+
+const Dashboard = React.lazy(() => import('./components/Dashboard'));
+const BlogListAdmin = React.lazy(() => import('./components/dashboard/BlogListAdmin'));
 
 function App() {
   const user = useSelector(selectCurrentUser)
-// console.log(user);
+
+
+
 
   return (
     <BrowserRouter>
       {user ? 'User Logged' : 'Not a session active'}
       <MainNavbar />
       <Routes>
-        {/* {user && <>
-          <Route path="/blogs/add" element={<BlogForm />} />
-        </>} */}
+       
         <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:id" element={<Blog />} />
         <Route path="/login" element={<Login />} />
 
         <Route element={<ProtectedRoute user={user} />}>
-          <Route path="/dashboard/*" element={<Dashboard />}>
+          <Route path="/dashboard/*" element={
+            <React.Suspense fallback={<>...</>}>
+              <Dashboard />
+            </React.Suspense>
+          }>
             <Route path="" element={<p>Contenidos</p>} />
-            <Route path="blogs" element={<BlogListAdmin/>} />
+            <Route path="blogs" element={
+              <React.Suspense fallback={<>...</>}>
+                <BlogListAdmin />
+              </React.Suspense>
+            } />
             <Route path="blogs/add" element={<BlogForm />} />
           </Route>
         </Route>
